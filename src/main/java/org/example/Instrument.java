a08d2c0a484e4d97ff98675eaf7ea3e66582f28c
oliverdroop <oliverdroop@hotmail.co.uk>
2023-12-04 23:38:47 +0000

    Add rhodes instrument

--- src/main/java/org/example/Instrument.java
+++ src/main/java/org/example/Instrument.java
@@ -11,6 +11,10 @@ public enum Instrument {
     TRIANGLE_TO_SINE((time, length) -> {
         double decayFactor = time / length;
         return (Waveform.TRIANGLE.calculate(time) * (1 - decayFactor)) + (Waveform.SINE.calculate(time) * decayFactor);
+    }),
+    RHODES((time, length) -> {
+        double volumeFactor = 1 - Math.pow(time / length, 2);
+        return ((Waveform.SINE.calculate(time)) + (Waveform.SINE.calculate(time / 2))) * volumeFactor;
     });
 
     private final BiFunction<Double, Double, Double> sampleCalculator;
